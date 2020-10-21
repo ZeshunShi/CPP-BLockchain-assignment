ce627ed08f3aa4b3ecf4e2f80b8172135f6e7e9c
ZeshunShi <44780304+ZeshunShi@users.noreply.github.com>
2020-10-21 18:28:21 +0200

    Update candidate's address

--- Voting_timeConstraints.sol
+++ Voting_timeConstraints.sol
@@ -28,11 +28,14 @@ contract Voting {
     constructor()
         public
     {       
-        candidatelist[0].name='Peter';       
-        candidatelist[0].votes=0;       
-        candidatelist[1].name='George';       
-        candidatelist[1].votes=0;       
-        candidatelist[2].name='Richard';       
+        candidatelist[0].name = 'Peter';       
+        candidatelist[0].ads = 0x68B87b5eb0908a23FC68788968a67A6d988aC255;       
+        candidatelist[0].votes = 0;       
+        candidatelist[1].name = 'George';       
+        candidatelist[1].ads = 0x72f4752ba880387b3E4b1c229599E348A5381Cb1;       
+        candidatelist[1].votes = 0;       
+        candidatelist[2].name = 'Richard';       
+        candidatelist[2].ads = 0x1727Fb67D31993494114d2fE171DC902085Cc323;       
         candidatelist[2].votes=0;   
     }     
 
